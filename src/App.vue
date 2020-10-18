aee525d38adc575e6c4a910e0adc5578fb4fd1c3
TimSusa <timmsusa@googlemail.com>
2020-10-18 04:55:06 +0200

    feature: Let Webmidi live with vue together

---
+++ src/App.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import WebMidi from 'webmidi'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  },
+  mounted(){
+    WebMidi.enable(function (err) {
+
+  if (err) {
+    console.log("WebMidi could not be enabled.", err);
+  } else {
+    console.log("WebMidi enabled!");
+        console.log(WebMidi.inputs);
+    console.log(WebMidi.outputs);
+  }
+  
+});
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
